8ff1b95668841a1d8e5a162c3ff367c642e6365f
Aibol Kussain <Aibol_Kussain@epam.com>
2017-12-15 15:55:51 +0300

    solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+var MAX_STEPS = 10000;
+
+function countSteps(race) {
+  var steps = 0;
+  var pos = 0;
+  while(pos < race.length && steps < MAX_STEPS) {
+    if (pos < 0) {
+      return MAX_STEPS;
+    }
+    pos = pos + race[pos];
+    steps++;
+  }
+  return steps;
+}
+
 module.exports = function getBestRacer(distance) {
-  // your solution here
+  var steps = [];
+  var min = MAX_STEPS + 1;
+  distance.forEach(function (item, index) {
+    var s = countSteps(item);
+    if (s < min) min = s;
+  });
+  return min;
 }
